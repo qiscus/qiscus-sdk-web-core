cba34d2c2dd9f53a2837c092f51890f4aeddcc84
desertlion <fikri.desertlion@gmail.com>
2018-03-09 09:45:12 +0700

    Fix room presence subscriber not called

--- index.js
+++ index.js
@@ -394,7 +394,7 @@ class QiscusSDK extends EventEmitter {
       }
     }
     const targetUserId = room.participants.filter(p => p.email != this.user_id);
-    if(room.type === 'single' && targetUserId.length > 0) this.realtimeAdapter.subscribeRoomPresence(targetUserId[0].email);
+    if(room.room_type === 'single' && targetUserId.length > 0) this.realtimeAdapter.subscribeRoomPresence(targetUserId[0].email);
     this.chatmateStatus = null;
     this.isTypingStatus = null;
     this.selected = room;
