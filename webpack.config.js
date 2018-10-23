e677bac16f3c9d1dd73eda21ca130d69a968e8dd
Afief Sholichuddin <apiep.oedin@gmail.com>
2018-10-23 14:29:40 +0700

    Update webpack and babel version

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,33 @@
-var webpack        = require('webpack');
-var path           = require('path');
-var libraryName    = 'QiscusSDKCore';
-// var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-var env            = process.env.WEBPACK_ENV;
-var plugins        = [], outputFile;
-const MinifyPlugin = require("babel-minify-webpack-plugin");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path')
 
-if (env === 'build') {
-  // plugins.push(new UglifyJsPlugin({ minimize: true }));
-  plugins.push(new MinifyPlugin({}, {comments: false}));
-  // plugins.push(new BundleAnalyzerPlugin());
-  outputFile = libraryName + '.min.js';
-} else {
-  // plugins.push(new BundleAnalyzerPlugin());
-  outputFile = libraryName + '.js';
-};
-
-var config = {
-  entry: ['babel-polyfill', __dirname + '/index.js'],
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/dist',
-    filename: outputFile,
-    library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    loaders: [
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ["env"]
+module.exports = (env) => {
+  const config = {
+    entry: ['@babel/polyfill', path.join(__dirname, 'index.js')],
+    devtool: env.production ? 'source-map' : 'eval',
+    mode: env.production ? 'production' : 'development',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: env.production ? 'qiscus-sdk-core.min.js' : 'qiscus-sdk-core.js',
+      library: 'QiscusSDKCore',
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['date-fns']
+          }
         },
         exclude: /(node_modules|bower_components)/
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: "eslint-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js']
-  },
-  plugins: plugins
-};
-
-module.exports = config;
+      }]
+    },
+    resolve: {
+      extensions: ['.js']
+    }
+  }
+  return config
+}
