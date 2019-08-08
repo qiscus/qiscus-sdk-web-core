cd381d15f16590673b512e17e30f8664601ec330
Afief Sholichuddin <afief@qiscus.co>
2019-08-08 12:01:42 +0700

    Fix adding message to wrong room

--- src/index.js
+++ src/index.js
@@ -82,7 +82,6 @@ class QiscusSDK {
    * @return {void}
    */
   init(config) {
-    console.log('qiscus.init')
     // set AppID
     if (!config.AppId) throw new Error('Please provide valid AppId')
     this.AppId = config.AppId
@@ -117,10 +116,13 @@ class QiscusSDK {
       if (this.selected != null) {
         const index = this.selected.comments
           .findIndex(it => it.id === message.id || it.unique_id === message.unique_temp_id)
+        console.log('message', message)
         if (index === -1) {
           const _message = new Comment(message)
-          this.selected.comments.push(_message)
-          this.sortComments()
+          if (_message.room_id === this.selected.id) {
+            this.selected.comments.push(_message)
+            this.sortComments()
+          }
           this.events.emit('newmessages', [message])
         }
       } else {
